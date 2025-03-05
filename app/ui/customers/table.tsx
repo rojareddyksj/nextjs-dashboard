1822a24d0df0aab0a14c509f523e8c075e116fc9
rojareddyksj <rojareddyksj@gmail.com>
2025-03-05 14:56:02 +0530

    changes

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+
 import Search from '@/app/ui/search';
 import {
-  CustomersTableType,
+
   FormattedCustomersTable,
 } from '@/app/lib/definitions';
+import { lusitana } from '@/app/ui/font';
+
 
 export default async function CustomersTable({
   customers,
