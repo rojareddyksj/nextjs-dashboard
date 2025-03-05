4b4a9babc25727ca9ca030f82f29eeba1e69a71b
rojareddyksj <rojareddyksj@gmail.com>
2025-03-05 13:00:01 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-700 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <a href="https://nextjs.org/learn/" className="text-blue-600">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
